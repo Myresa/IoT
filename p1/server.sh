f0f4507ccbaf84cce4d9bc473814977a13e6b2c7
Myresa <32522234+Myresa@users.noreply.github.com>
2025-07-03 17:48:23 +0200

    Update server.sh - Fix token access

--- p1/server.sh
+++ p1/server.sh
@@ -10,6 +10,9 @@ export INSTALL_K3S_EXEC="--flannel-iface=eth1"
 #Install K3s server
 curl -sfL https://get.k3s.io | sh -
 
+#Make the auth token available for worker nodes
+cp /var/lib/rancher/k3s/server/token /vagrant/token
+
 # wait for node to be ready
 until kubectl get nodes | grep -q " Ready"; do
     echo "Waiting for node to be ready..."
